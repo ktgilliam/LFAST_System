c9b206265860604febe350d6eeaea8db6ff499cc
Kevin Gilliam <keving@arizona.edu>
2023-02-14 11:56:59 -0700

    updating to reflect current system configuration

---
+++ PowerType.m
@@ -0,0 +1,9 @@
+classdef PowerType < Simulink.IntEnumType
+    % PowerType Enumeration type definition for use with System Composer profile
+
+    enumeration
+        AC(0)
+        DC(1)
+    end
+
+end
---
+++ SoftwareType.m
@@ -0,0 +1,9 @@
+classdef SoftwareType < Simulink.IntEnumType
+    % SoftwareType Enumeration type definition for use with System Composer profile
+
+    enumeration
+        Python(0)
+        INDI(1)
+    end
+
+end
